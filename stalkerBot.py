3d864b7bf9f803974e083d70543dc1ee74289ecd
Guillaume Binet <gbin@google.com>
2015-05-27 16:34:47 -0700

    updated this plugin to modern err.

--- stalkerBot.py
+++ stalkerBot.py
@@ -1,35 +1,27 @@
-from errbot.utils import get_jid_from_message,format_timedelta
+from errbot.utils import format_timedelta
 from datetime import datetime
-
-# Backward compatibility
-from errbot.version import VERSION
-from errbot.utils import version2array
-if version2array(VERSION) >= [1,6,0]:
-    from errbot import botcmd, BotPlugin
-else:
-    from errbot.botplugin import BotPlugin
-    from errbot.jabberbot import botcmd
-
+from errbot import botcmd, BotPlugin
+import logging
+log = logging.getLogger(__name__)
 
 class StalkerBot(BotPlugin):
-    def callback_message(self, conn, mess):
+    def callback_message(self, mess):
         message = mess.getBody()
         if not message:
             return
 
-        username = get_jid_from_message(mess)
-        self.shelf[username] = datetime.now()
-        self.shelf.sync()
-
+        username = mess.frm.node
+        log.debug("Recording presence of %s", username)
+        self[username] = datetime.now()
 
     @botcmd
     def seen(self, mess, args):
         """ find out when someone last said something """
-        username = get_jid_from_message(mess)
+        username = mess.frm.node
         if username == args:
             return 'I can see you now'
         try:
-            last_seen = self.shelf[str(args)]
+            last_seen = self[str(args)]
             return 'I last saw %s %s ago (on %s)' % (args, format_timedelta(datetime.now() - last_seen), datetime.strftime(last_seen, '%A, %b %d at %H:%M'))
         except KeyError:
             return 'I have no record of %s' % args
